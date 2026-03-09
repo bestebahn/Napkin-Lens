477f95f9fadb78b956c8948766ab87f8677b0ce0
bestebahn <efcarbajal1@gmail.com>
2026-03-09 14:20:36 -0700

    Change default port to 5001 and streamline requirements

--- app.py
+++ app.py
@@ -260,4 +260,4 @@ def export_csv():
 # -- Run ----------------------------------------------------------------------
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=5001)
